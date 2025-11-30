47f87300537380b42cf51e2d777c7b54d2aafa40
LifelongWay <amin.abuhilga.140705@gmail.com>
2025-11-30 15:27:09 +0300

    swinb added(demo)

--- 2022400336/descriptor.py
+++ 2022400336/descriptor.py
@@ -3,10 +3,20 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+# for swinb
+from PIL import Image
+import torch
+from torchvision.models import swin_b, Swin_B_Weights
+
+
+
+##      -- sift --       ##
+
 # checking existance of dir for saving extracted sift featuers
-REL_SAVING_PATH = '../data/descriptors/sift'
-SIFT_DESCRIPTOR_DIR = os.path.join(os.getcwd(), REL_SAVING_PATH)
+SIFT_REL_SAVING_PATH = '../data/descriptors/sift'
+SIFT_DESCRIPTOR_DIR = os.path.join(os.getcwd(), SIFT_REL_SAVING_PATH)
 os.makedirs(SIFT_DESCRIPTOR_DIR, exist_ok=True) # Ensure directory exists
+
 
 # extracting sift features for given filepaths.txt
 def extract_sift_features(img_list_file_path, imgs_root_path):
@@ -43,5 +53,67 @@ def extract_sift_features(img_list_file_path, imgs_root_path):
         # getting filename 
         npy_filename = os.path.basename(img_path.rsplit('.', 1)[0] + '.npy')
         
-        # save descriptors in REL_SAVING_PATH as filename.npy
-        np.save(os.path.join(REL_SAVING_PATH, npy_filename) , descriptors)
+        # save descriptors in sift_REL_SAVING_PATH as filename.npy
+        np.save(os.path.join(SIFT_REL_SAVING_PATH, npy_filename) , descriptors)
+
+
+
+##      -- swin_b --       ##
+SWIN_REL_SAVING_PATH = '../data/descriptors/swin'
+SWIN_DESCRIPTOR_DIR = os.path.join(os.getcwd(), SWIN_REL_SAVING_PATH)
+os.makedirs(SIFT_DESCRIPTOR_DIR, exist_ok=True) # Ensure directory exists
+
+
+def extract_swin_b_features(img_list_file_path, imgs_root_path):
+    # loading pretrained weights from IMAGENET1K_V1
+    weights = Swin_B_Weights.IMAGENET1K_V1
+    preprocess = weights.transforms()
+
+    # loading feature extractor
+    model = swin_b(weights = weights)
+    model = model.features
+    model.eval()
+
+    # loop over image paths
+    with open(img_list_file_path, 'r') as f:
+        img_paths = [p.strip() for p in f if p.strip()]
+        
+    img_paths = img_paths[0:30]
+    for img_path in img_paths:
+
+        # reading only with img-extensions
+        if not img_path.endswith(('jpg', 'jpeg', 'png')):
+            continue
+
+        full_path = os.path.join(imgs_root_path, img_path)
+
+        # try loading image
+        try:
+            img = Image.open(full_path).convert("RGB")
+        except Exception:
+            print(f"[Error] PIL couldn't read image at {full_path}")
+            continue
+
+        # apply pretrained transforms
+        img_tensor = preprocess(img).unsqueeze(0)  # shape (1, 3, 224, 224)
+
+        # forward pass
+        with torch.no_grad():
+            feats = model(img_tensor)        # (1, 1024, 7, 7)
+
+        # convert to shape (49, 1024)
+        feats = feats.squeeze(0)             # (1024, 7, 7)
+        feats = feats.flatten(1).T           # (49, 1024)
+        descriptors = feats.cpu().numpy()
+
+        if descriptors.shape[0] == 0:
+            print(f'[Error] No descriptors found at {img_path}!')
+            continue
+
+        # build output filename (replace .jpg → .npy)
+        npy_filename = os.path.basename(img_path.rsplit('.', 1)[0] + '.npy')
+
+        # save to directory
+        np.save(os.path.join(SWIN_REL_SAVING_PATH, npy_filename), descriptors)
+
+        print(f"[OK] Saved {descriptors.shape} descriptors for {img_path}")
